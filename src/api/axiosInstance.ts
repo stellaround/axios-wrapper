453f9c22b2d4046ad93f6572ab7a9bb825bd6ea5
杜帅琪 <1109511798@qq.com>
2024-07-30 14:41:36 +0800

    fix: 删除无用代码

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { type AxiosRequestConfig, type AxiosResponse } from 'axios';
 
 
 export interface ResponseBody<T = any> {
--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   build: {
+    outDir: 'dist', // 确保输出目录
     lib: {
-      entry: 'src/api/index.ts',
-      name: 'AxiosWrapper',
-      fileName: (format) => `axios-wrapper.${format}.js`
+      entry: './src/api/index.ts',
+      name: 'index',
+      fileName: (format) => `index.${format}.js`
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
@@ -18,7 +19,8 @@ export default defineConfig({
           vue: 'Vue',
           axios: 'axios',
           'ant-design-vue': 'antd'
-        }
+        },
+        assetFileNames: 'assets/[name].[ext]',
       }
     }
   }
